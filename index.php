cd00ce5e8a894f824843960948260725f87a4507
Devfrank94 <francescomurro94@gmail.com>
2023-05-17 17:53:18 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
+  <title>PHP Strong Password Generator</title>
+</head>
+
+<body>
+  <div class="wrapper container d-flex flex-column justify-content-center align-items-center">
+    <header class="text-center pb-4">
+      <h1>Genera la tua password</h1>
+
+    </header>
+    <main>
+
+    </main>
+  </div>
+</body>
+
+</html>
